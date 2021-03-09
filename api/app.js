03d1370bfd5fa90d736d90b2d53b87913ccef2f7
sameerdewan <sameer.dewan@itential.com>
2021-03-09 15:40:42 -0500

    stub

---
+++ api/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+// const route = require('../routes/poetry');
+const app = express();
+
+// app.use('/api/route', route);
+
+module.exports = app;
---
+++ api/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Listening on port ${process.env.PORT || 8080}...`);
+});

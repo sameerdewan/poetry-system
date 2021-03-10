d549657257f21b8ec61aff9907351481fdf7f0b3
sameerdewan <sameer.dewan@itential.com>
2021-03-10 11:15:12 -0500

    add middleware fn

--- jwt/index.js
+++ jwt/index.js
@@ -21,6 +21,18 @@ class PoetrySystemJWT {
     verifyToken(token) {
         return jwt.verify(token, this.publicKey);
     }
+
+    middleware(req, res, next) {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        try {
+            const user = this.verifyToken(token);
+            req.user = user;
+        } catch (error) {
+            res.status(403).json({ error: error.message });
+        }
+        next();
+    }
 }
 
 module.exports = PoetrySystemJWT;

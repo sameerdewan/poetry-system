297660a2fec7325640c5c8536f907aa570b3ed55
sameerdewan <sameer.dewan@itential.com>
2021-03-14 22:59:39 -0400

    pushed system work

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const registration = require('../routes/registration');
 const login = require('../routes/login');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.options('*', cors());
 
 app.use('/api/registration', registration);
 app.use('/api/login', login);
